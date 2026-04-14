43580ec6534b680744e4870cdaad0181793c85f6
Your Name <you@example.com>
2026-04-13 21:34:44 -0600

    se cambio el icono de login 3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
     
     <!-- Sección de imagen -->
     <div class="w-1/2 bg-gradient-to-b from-[#010e6b] via-[#be74be] via-[#d481d2] to-[#703b94] flex items-center justify-center">
-      <img src="http://admin.umbrellastella.com/assets/iconos/logo.png" alt="Login Image" class="rounded-lg shadow-lg max-h-[80%]">
+<img src="{{ asset('storage/imagenes/pagina/logo.png') }}" alt="Login Image" class="rounded-lg shadow-lg max-h-[80%]">
     </div>
 
     <!-- Sección de formulario -->
